f086824c8ba81f6c6f15165a18ac97b5479a8181
jmac-94 <josue.arriaga@utec.edu.pe>
2023-11-17 16:22:16 -0500

    refactor

--- search_query.py
+++ search_query.py
@@ -3,14 +3,14 @@ import time
 from inv_index_merger import InvIndexMerger
 
 if __name__ == '__main__':
-    divider_merger = InvIndexMerger()
+    merger = InvIndexMerger()
 
     query = input('Query: ')
     k = int(input('Top k: '))
     language = str(input('Language abbreviation: '))
 
     start_time = time.time()
-    results = divider_merger.search_query_merged_blocks(query, k, language)
+    results = merger.search_query_merged_blocks(query, k, language)
     end_time = time.time()
 
     elapsed_time = end_time - start_time
